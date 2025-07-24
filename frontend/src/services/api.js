0082288a7904350dd7c59ff557f0b190771f4453
albz007 <albinjoseph229@gmail.com>
2025-07-24 23:58:22 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -13,4 +13,14 @@ apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      // If a token exists, add it to the A
+      // If a token exists, add it to the Authorization header
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
